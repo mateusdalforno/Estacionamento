a43b831254bd203d2ca77e71dc7620e8ef03517d
mateu <mateu@Dell7472>
2022-10-06 16:48:47 -0300

    semana 2 - classes de conexão

---
+++ Estacionamento/src/connection/script.sql
@@ -0,0 +1,19 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
+ */
+/**
+ * Author:  mateu
+ * Created: 6 de out. de 2022
+ */
+
+CREATE DATABASE IF NOT EXISTS estacionamento;
+USE estacionamento;
+
+CREATE TABLE vaga (
+    idVaga  int NOT NULL AUTO_INCREMENT,
+    numero  int NOT NULL,
+    rua     varchar(100) NOT NULL,
+    obliqua boolean NOT NULL,
+    PRIMARY KEY (idVaga));
+
