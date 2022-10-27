d30298c596a795a6533c9e0a92f50d4b3b3e1985
02664899074 <02664899074@LAB205-01.fw.iffar.edu.br>
2022-10-27 19:58:25 -0300

    Semana 5 - Listagem das vagas

--- Estacionamento/src/connection/ConnectionFactory.java
+++ Estacionamento/src/connection/ConnectionFactory.java
@@ -14,7 +14,7 @@ import java.sql.ResultSet;
 public class ConnectionFactory {
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = 
-            "jdbc:mysql://localhost:3306/estacionamento";
+            "jdbc:mysql://localhost:3307/estacionamento";
     public static final String USER = "root";
     public static final String PASS = "";
     
